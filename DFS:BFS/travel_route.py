ea311a373cff7c03c94121fe2452582f11241a46
Lee424 <tmdghks424@naver.com>
2020-10-01 01:05:47 +0900

    [Add] 여행경로

---
+++ DFS:BFS/travel_route.py
@@ -0,0 +1,27 @@
+import collections, copy
+
+def solution(tickets):
+    graph = collections.defaultdict(list)
+
+    for ticket in tickets:
+        graph[ticket[0]].append(ticket[1])
+
+    queue = collections.deque([('ICN', ['ICN'])])
+    result = []
+
+    def find_path(graph, queue):
+        n, path = queue.popleft()
+
+        if len(path) == len(tickets)+1:
+            result.append(path)
+            return
+
+        for m in graph[n]:
+            next_graph = copy.deepcopy(graph)
+            queue.append((m, path + [m]))
+            next_graph[n].remove(m)
+            find_path(next_graph,queue)
+
+    find_path(graph,queue)
+    result.sort()
+    return result[0]
