e5e00f082bcca9322e7d7ce0950eb9199d5f45b0
Lee424 <tmdghks424@naver.com>
2021-05-25 15:01:37 +0900

    [Add] 올바른 괄호

---
+++ Stack:Queue/correct_bracket.py
@@ -0,0 +1,14 @@
+def solution(s):
+    stack = []
+    for i in s:
+        if i == '(':
+            stack.append(i)
+        else:
+            if stack:
+                stack.pop()
+            else:
+                return False
+    if not stack:
+        return True
+
+    return False
